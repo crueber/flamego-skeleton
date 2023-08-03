7f5f6888f93af1f86900e7900d44514ea51e6d63
Chris Rueber <crueber@gmail.com>
2023-08-03 09:45:17 -0500

    Template updates.

--- models/user.go
+++ models/user.go
@@ -1,35 +1,34 @@
 package models
 
 import (
-  "gorm.io/gorm"
+	"gorm.io/gorm"
 )
 
 type User struct {
-  gorm.Model
-  ID uint `gorm:"primaryKey,autoIncrement"`
-  Name string `form:"name" json:"name" validate:"required"`
-  Email string `form:"email" json:"email" validate:"required,email"`
+	gorm.Model
+	ID    uint   `gorm:"primaryKey,autoIncrement"`
+	Name  string `form:"name" json:"name" validate:"required"`
+	Email string `form:"email" json:"email" validate:"required,email"`
 }
 
 func (u *User) Save(db *gorm.DB) error {
-  result := db.Save(&u)
-  return result.Error
+	result := db.Save(&u)
+	return result.Error
 }
 
 func (u *User) Delete(db *gorm.DB) error {
-  result := db.Delete(&u)
-  return result.Error
+	result := db.Delete(&u)
+	return result.Error
 }
 
 func ListAllUsers(db *gorm.DB) []User {
-  var users []User
-  db.Order("id desc").Find(&users)
-  return users
+	var users []User
+	db.Order("id desc").Find(&users)
+	return users
 }
 
 func GetUser(db *gorm.DB, id int) User {
-  var user User
-  db.First(&user, id)
-  return user
+	var user User
+	db.First(&user, id)
+	return user
 }
-
